d8d43311778268f7431c641abcc30c145d64ad96
Dharsan_R <dharsanravichandran1@gmail.com>
2022-04-21 13:19:56 +0530

    adding trv file

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception {
 
+    //property
     private static final long serialVersionUID=1L;
 
     public ResourceNotFoundException(String message){
